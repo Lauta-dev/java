e73d5ff3bd808b1b190c0954e572aaa9c0650ab7
lautaro <you@example.com>
2023-11-04 11:14:15 -0300

    Agregado: paquete dotenv para leer archivos .env

--- src/main/java/com/ejemplo/Sql.java
+++ src/main/java/com/ejemplo/Sql.java
@@ -3,10 +3,18 @@ package com.ejemplo;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 import com.ejemplo.sql.sql;
 
 public class Sql {
   public static void main(String[] args) throws SQLException {
+    Dotenv dotenv = Dotenv.load();
+
+    String jdbcUrl = dotenv.get("DB_LOCALHOST");
+    String usuario = dotenv.get("DB_USER");
+    String contraseña = dotenv.get("DB_PASS");
+
     String consulta = "SELECT * FROM users";
 
     ResultSet result = sql.Resultado(jdbcUrl, usuario, contraseña, consulta);
